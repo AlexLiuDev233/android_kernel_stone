d7c3c62f87794b8a34332731f6885ea8fcf472b4
Vivek Goyal <vgoyal@redhat.com>
2020-08-19 18:19:45 -0400

    virtiofs: get rid of no_mount_options
    This option was introduced so that for virtio_fs we don't show any mounts
    options fuse_show_options(). Because we don't offer any of these options
    to be controlled by mounter.
    
    Very soon we are planning to introduce option "dax" which mounter should
    be able to specify. And no_mount_options does not work anymore.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -505,7 +505,7 @@ struct fuse_fs_context {
 	bool destroy:1;
 	bool no_control:1;
 	bool no_force_umount:1;
-	bool no_mount_options:1;
+	bool legacy_opts_show:1;
 	unsigned int max_read;
 	unsigned int blksize;
 	const char *subtype;
@@ -632,6 +632,9 @@ struct fuse_conn {
 
 	/** cache READLINK responses in page cache */
 	unsigned cache_symlinks:1;
+
+	/* show legacy mount options */
+	unsigned int legacy_opts_show:1;
 
 	/*
 	 * The following bitfields are only for optimization purposes
