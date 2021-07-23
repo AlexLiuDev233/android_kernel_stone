2cf4466cd9d72c566b981ba908c0acd3b6fc077b
Shaleen Agrawal <shalagra@codeaurora.org>
2021-07-22 17:09:23 -0700

    ANDROID: sched: add restricted hook to override show_cpufinfo_max_freq
    Need to update to restricted hook since the module code registering for
    this hook needs to take the task lock and walk over the linked
    libraries.
    
    Bug: 194431527
    Change-Id: I7093b66f95c3b9c473386ade60d8897426ddbb13
    Signed-off-by: Shaleen Agrawal <shalagra@codeaurora.org>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -715,7 +715,7 @@ static ssize_t show_cpuinfo_max_freq(struct cpufreq_policy *policy, char *buf)
 {
 	unsigned int freq = policy->cpuinfo.max_freq;
 
-	trace_android_vh_show_max_freq(policy, &freq);
+	trace_android_rvh_show_max_freq(policy, &freq);
 
 	if (should_use_cached_freq(policy->cpu))
 		freq = cpuinfo_max_freq_cached << 1;
--- include/trace/hooks/cpufreq.h
+++ include/trace/hooks/cpufreq.h
@@ -13,9 +13,9 @@
 
 #if defined(CONFIG_TRACEPOINTS) && defined(CONFIG_ANDROID_VENDOR_HOOKS)
 
-DECLARE_HOOK(android_vh_show_max_freq,
+DECLARE_RESTRICTED_HOOK(android_rvh_show_max_freq,
 	TP_PROTO(struct cpufreq_policy *policy, unsigned int *max_freq),
-	TP_ARGS(policy, max_freq));
+	TP_ARGS(policy, max_freq), 1);
 
 DECLARE_HOOK(android_vh_freq_table_limits,
 	TP_PROTO(struct cpufreq_policy *policy, unsigned int min_freq,
