a937054c32c91a1fb62f16b0ddc50e84a70ef522
AlexLiuDev233 <wzylin11@outlook.com>
2025-03-21 23:57:30 +0800

    Revert "kernel: Integrate Re-Kernel"
    This reverts commit a26fa2716cd85aa9378e6799d3b2e60fa8e23424.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3015,15 +3015,6 @@ static struct binder_node *binder_get_node_refs_for_txn(
 	return target_node;
 }
 
-#ifdef CONFIG_ANDROID_REKERNEL
-#define PACKET_SIZE 				128
-#define MAX_SYSTEM_UID  			(2000)
-#define MIN_USERAPP_UID 			(10000)
-extern bool line_is_frozen(struct task_struct *task);
-extern int start_rekernel_server(void);
-extern int send_rekernel_netlink_message(char *msg, uint16_t len);
-#endif
-
 static void binder_transaction(struct binder_proc *proc,
 			       struct binder_thread *thread,
 			       struct binder_transaction_data *tr, int reply,
@@ -3125,20 +3116,6 @@ static void binder_transaction(struct binder_proc *proc,
 		target_proc = target_thread->proc;
 		target_proc->tmp_ref++;
 		binder_inner_proc_unlock(target_thread->proc);
-		#ifdef CONFIG_ANDROID_REKERNEL
-		if (start_rekernel_server() == 0) {
-			if (target_proc
-				&& (NULL != target_proc->tsk)
-				&& (NULL != proc->tsk)
-				&& (task_uid(target_proc->tsk).val <= MAX_SYSTEM_UID)
-				&& (proc->pid != target_proc->pid)
-				&& line_is_frozen(target_proc->tsk)) {
-     				char binder_kmsg[PACKET_SIZE];
-                       	snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Binder,bindertype=reply,oneway=0,from_pid=%d,from=%d,target_pid=%d,target=%d;", proc->pid, task_uid(proc->tsk).val, target_proc->pid, task_uid(target_proc->tsk).val);
-         			send_rekernel_netlink_message(binder_kmsg, strlen(binder_kmsg));
-			}
-   		}
-		#endif
 	} else {
 		if (tr->target.handle) {
 			struct binder_ref *ref;
@@ -3191,22 +3168,6 @@ static void binder_transaction(struct binder_proc *proc,
 			goto err_dead_binder;
 		}
 		e->to_node = target_node->debug_id;
-
-		#ifdef CONFIG_ANDROID_REKERNEL
-		if (start_rekernel_server() == 0) {
-			if (target_proc
-				&& (NULL != target_proc->tsk)
-				&& (NULL != proc->tsk)
-				&& (task_uid(target_proc->tsk).val > MIN_USERAPP_UID)
-				&& (proc->pid != target_proc->pid)
-				&& line_is_frozen(target_proc->tsk)) {
-     				char binder_kmsg[PACKET_SIZE];
-                       	snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Binder,bindertype=transaction,oneway=%d,from_pid=%d,from=%d,target_pid=%d,target=%d;", tr->flags & TF_ONE_WAY, proc->pid, task_uid(proc->tsk).val, target_proc->pid, task_uid(target_proc->tsk).val);
-         			send_rekernel_netlink_message(binder_kmsg, strlen(binder_kmsg));
-			}
-   		}
-		#endif
-
 		if (security_binder_transaction(binder_get_cred(proc),
 						binder_get_cred(target_proc)) < 0) {
 			return_error = BR_FAILED_REPLY;
--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -388,16 +388,6 @@ static bool debug_low_async_space_locked(struct binder_alloc *alloc, int pid)
 	return false;
 }
 
-#ifdef CONFIG_ANDROID_REKERNEL
-#define PACKET_SIZE 				128
-#define MAX_SYSTEM_UID  			(2000)
-#define RESERVE_ORDER				17
-#define WARN_AHEAD_SPACE			(1 << RESERVE_ORDER)
-extern bool line_is_frozen(struct task_struct *task);
-extern int start_rekernel_server(void);
-extern int send_rekernel_netlink_message(char *msg, uint16_t len);
-#endif
-
 static struct binder_buffer *binder_alloc_new_buf_locked(
 				struct binder_alloc *alloc,
 				size_t data_size,
@@ -406,10 +396,6 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 				int is_async,
 				int pid)
 {
-	#ifdef CONFIG_ANDROID_REKERNEL
-	struct task_struct *proc_task = NULL;
-	#endif
-
 	struct rb_node *n = alloc->free_buffers.rb_node;
 	struct binder_buffer *buffer;
 	size_t buffer_size;
@@ -442,23 +428,6 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 				alloc->pid, extra_buffers_size);
 		return ERR_PTR(-EINVAL);
 	}
-
-	#ifdef CONFIG_ANDROID_REKERNEL
-	if (is_async
-		&& (alloc->free_async_space < 3 * (size + sizeof(struct binder_buffer))
-		|| (alloc->free_async_space < WARN_AHEAD_SPACE))) {
-		rcu_read_lock();
-		proc_task = find_task_by_vpid(alloc->pid);
-		rcu_read_unlock();
-		if (proc_task != NULL && start_rekernel_server() == 0) {
-			if (line_is_frozen(proc_task)) {
-     				char binder_kmsg[PACKET_SIZE];
-                       	snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Binder,bindertype=free_buffer_full,oneway=1,from_pid=%d,from=%d,target_pid=%d,target=%d;", current->pid, task_uid(current).val, proc_task->pid, task_uid(proc_task).val);
-         			send_rekernel_netlink_message(binder_kmsg, strlen(binder_kmsg));
-			}
-		}
-	}
-	#endif
 
 	/* Pad 0-size buffers so they get assigned unique addresses */
 	size = max(size, sizeof(void *));
