bd99ee4092c7dddb7a6193e1fdf5663aa30a048b
AlexLiuDev233 <wzylin11@outlook.com>
2025-07-25 09:26:16 +0800

    kernel: susfs: Remove support for Shamiko
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -21,11 +21,6 @@
 #include "pnode.h"
 #include "internal.h"
 
-#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern bool susfs_is_current_ksu_domain(void);
-#endif
-
-
 #ifdef CONFIG_KSU_SUSFS
 #include <linux/susfs.h>
 #endif
@@ -115,7 +110,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID) && !susfs_is_current_ksu_domain()))
+	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID)))
 		return 0;
 #endif
 
@@ -160,7 +155,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int status = 1;
 #endif
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID) && !susfs_is_current_ksu_domain()))
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID))
 		return 0;
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT_MNT_ID_REORDER
 	if (!uid_matches_proc_need_to_reorder_mnt_id())
@@ -243,7 +238,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely((r->mnt_id >= DEFAULT_SUS_MNT_ID) && !susfs_is_current_ksu_domain()))
+	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID))
 		return 0;
 #endif
 
